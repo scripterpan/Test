c3e8062bf6b261dc52bf1fdfef70f44a1b52f115
Pann <136094314+scripterpan@users.noreply.github.com>
2025-09-09 17:56:37 +0700

    Update and rename getgenv-test.luau to getgenv-test.lua

--- getgenv-test.lua
+++ getgenv-test.lua
@@ -1,12 +1,17 @@
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 if getgenv().Notify then
-  WindUI:Notify({
-    Title = "getgenv.test()",
-    Content = "Mangos!",
-    Duration = 3, -- 3 seconds
-    Icon = "bird",
-})
+  task.spawn(function()
+      while true do
+        WindUI:Notify({
+            Title = "getgenv.test()",
+            Content = "Mangos!",
+            Duration = 3, -- 3 seconds
+            Icon = "bird",
+        })
+        task.wait(0.2)
+      end
+  end)
 end
 
 if getgenv().Kick then
@@ -18,7 +23,7 @@ if getgenv().JameHumYan then
     task.spawn(function()
         while true do
             setclipboard("discord.gg/jamehumyan")
-            task.wait(0.1)
+            task.wait(0.5)
         end
     end)
 end
